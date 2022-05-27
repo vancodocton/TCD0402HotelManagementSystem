b0e0d28b8f4df3b31d1df33e11e09ff00d469ca6
Vinh Hoang <vinhhn3@fe.edu.vn>
2022-05-27 09:26:31 +0700

    Add project files.

---
+++ HotelManagementSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HotelManagementSystem
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
